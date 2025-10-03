cf47ad19e6c8b57317a18d005c3e98fc66bf75c1
Wissam Zeh <zehmani54@gmail.com>
2025-10-03 15:49:10 +0200

    nb de tâches

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { tasksCollection } from './data'
 
 function App() {
-  
+  const tasks = tasksCollection ;
 
   return (
     <>
     <h1>Todo List</h1>
+    <p>Nombre de tâches : {tasks.length}</p>
     </>
   )
 }
